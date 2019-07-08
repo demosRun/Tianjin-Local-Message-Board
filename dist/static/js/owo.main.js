b31de3431c9fa97ae3141aeceb220a61b70320a7
puge <mail@puge.org>
2019-07-08 14:51:46 +0800

    初步确认

--- dist/static/js/owo.main.js
+++ dist/static/js/owo.main.js
@@ -1,4 +1,4 @@
-// Thu Jul 04 2019 15:44:42 GMT+0800 (GMT+08:00)
+// Mon Jul 08 2019 14:41:26 GMT+0800 (GMT+08:00)
 
 "use strict";
 
@@ -227,103 +227,20 @@ var _owo = {
   },
 
   /* 运行页面初始化方法 */
-  runCreated: function runCreated(pageFunction, entryDom) {
+  runCreated: function runCreated(pageFunction) {
     pageFunction.created.apply(_owo.assign(pageFunction, {
-      $el: entryDom,
       data: pageFunction.data,
       activePage: window.owo.activePage
     }));
-  },
-
-  /* 注册事件监听 */
-  registerEvent: function registerEvent(pageFunction, entryDom) {
-    // 判断是否包含事件监听
-    if (pageFunction.event) {
-      if (!window.owo.state.event) window.owo.state.event = {};
-
-      for (var iterator in pageFunction.event) {
-        if (!window.owo.state.event[iterator]) window.owo.state.event[iterator] = [];
-        window.owo.state.event[iterator].push({
-          dom: entryDom,
-          pageName: window.owo.activePage,
-          fun: pageFunction.event[iterator],
-          script: pageFunction
-        });
-      }
-    }
   }
+  /* owo事件处理 */
+  // 参数1: 当前正在处理的dom节点
+  // 参数2: 当前正在处理的模块名称
+  // 参数3: 当前正在处理的模块根dom
+
 };
 
-if (!document.getElementsByClassName) {
-  /* 解决低版本浏览器没有getElementsByClassName的问题 */
-  document.getElementsByClassName = function (className, element) {
-    var children = (element || document).getElementsByTagName('*');
-    var elements = new Array();
-
-    for (var i = 0; i < children.length; i++) {
-      var child = children[i];
-      var classNames = child.className.split(' ');
-
-      for (var j = 0; j < classNames.length; j++) {
-        if (classNames[j] == className) {
-          elements.push(child);
-          break;
-        }
-      }
-    }
-
-    return elements;
-  };
-}
-/* 运行页面所属的方法 */
-
-
-_owo.handlePage = function (pageName, entryDom) {
-  _owo.handleEvent(entryDom, null, entryDom);
-  /* 判断页面是否有自己的方法 */
-
-
-  var newPageFunction = window.owo.script[pageName];
-  if (!newPageFunction) return; // console.log(newPageFunction)
-  // 如果有created方法则执行
-
-  if (newPageFunction.created) {
-    _owo.runCreated(newPageFunction, entryDom);
-  } // 注册事件监听
-
-
-  _owo.registerEvent(newPageFunction, entryDom); // 判断页面是否有下属模板,如果有运行所有模板的初始化方法
-
-
-  for (var key in newPageFunction.template) {
-    var templateScript = newPageFunction.template[key];
-
-    if (templateScript.created) {
-      // 获取到当前配置页的DOM
-      // 待修复,临时获取方式,这种方式获取到的dom不准确
-      var domList = document.querySelectorAll('[template="' + key + '"]'); // 有时候在更改html时会将某些块进行删除
-
-      if (domList.length == 0) {
-        console.info('无法找到页面组件:' + key);
-      } // console.log(domList.length)
-
-
-      for (var ind = 0; ind < domList.length; ind++) {
-        _owo.runCreated(templateScript, domList[ind]); // 注册事件监听
-
-
-        _owo.registerEvent(templateScript, domList[ind]);
-      }
-    }
-  }
-};
-/* owo事件处理 */
-// 参数1: 当前正在处理的dom节点
-// 参数2: 当前正在处理的模块名称
-// 参数3: 当前正在处理的模块根dom
-
-
-_owo.handleEvent = function (tempDom, templateName, entryDom) {
+_owo.handleEvent = function (tempDom, templateName) {
   var activePage = window.owo.script[owo.activePage];
 
   if (tempDom.attributes) {
@@ -357,17 +274,17 @@ _owo.handleEvent = function (tempDom, templateName, entryDom) {
                 // 判断页面是否有自己的方法
                 var newPageFunction = window.owo.script[window.owo.activePage]; // console.log(this.attributes)
 
-                if (this.templateName !== owo.activePage) {
+                if (templateName && templateName !== owo.activePage) {
                   // 如果模板注册到newPageFunction中，那么证明模板没有script那么直接使用eval执行
                   if (newPageFunction.template) {
-                    newPageFunction = newPageFunction.template[this.templateName];
+                    newPageFunction = newPageFunction.template[templateName];
                   }
                 } // 待优化可以单独提出来
                 // 取出参数
 
 
                 var parameterArr = [];
-                var parameterList = this.eventFor.match(/[^\(\)]+(?=\))/g);
+                var parameterList = eventFor.match(/[^\(\)]+(?=\))/g);
 
                 if (parameterList && parameterList.length > 0) {
                   // 参数列表
@@ -387,31 +304,23 @@ _owo.handleEvent = function (tempDom, templateName, entryDom) {
 
                   }
 
-                  this.eventFor = this.eventFor.replace('(' + parameterList + ')', '');
+                  eventFor = eventFor.replace('(' + parameterList + ')', '');
                 } else {
                   // 解决 @click="xxx()"会造成的问题
-                  this.eventFor = this.eventFor.replace('()', '');
+                  eventFor = eventFor.replace('()', '');
                 } // console.log(newPageFunction)
                 // 如果有方法,则运行它
 
 
-                if (newPageFunction[this.eventFor]) {
+                if (newPageFunction[eventFor]) {
                   // 绑定window.owo对象
-                  // console.log(tempDom)
-                  // 待测试不知道这样合并会不会对其它地方造成影响
-                  newPageFunction.$el = this.entryDom;
                   newPageFunction.$event = event;
-                  newPageFunction[this.eventFor].apply(newPageFunction, parameterArr);
+                  newPageFunction[eventFor].apply(newPageFunction, parameterArr);
                 } else {
                   // 如果没有此方法则交给浏览器引擎尝试运行
-                  eval(this.eventFor);
+                  eval(eventFor);
                 }
-              }.bind({
-                eventFor: eventFor,
-                templateName: templateName,
-                entryDom: entryDom,
-                tempDom: tempDom
-              });
+              };
             }
         }
       }
@@ -428,11 +337,9 @@ _owo.handleEvent = function (tempDom, templateName, entryDom) {
       if (childrenDom.attributes['template'] && childrenDom.attributes['template'].textContent) {
         // 如果即将遍历进入模块 设置即将进入的模块为当前模块
         // 获取模块的模块名
-        templateName = childrenDom.attributes['template'].textContent;
-
-        _owo.handleEvent(childrenDom, templateName, childrenDom);
+        _owo.handleEvent(childrenDom, childrenDom.attributes['template'].textContent);
       } else {
-        _owo.handleEvent(childrenDom, templateName, entryDom);
+        _owo.handleEvent(childrenDom, templateName);
       }
     }
   } else {
@@ -442,25 +349,17 @@ _owo.handleEvent = function (tempDom, templateName, entryDom) {
 }; // 页面资源加载完毕事件
 
 
-_owo.ready = function () {
+_owo.showPage = function () {
   var page = owo.entry;
   window.owo.activePage = page;
   var entryDom = document.getElementById('o-' + page);
 
   if (entryDom) {
-    _owo.handlePage(page, entryDom);
+    _owo.handlePage(window.owo.script[page], entryDom);
+
+    _owo.handleEvent(entryDom, null);
   } else {
     console.error('找不到页面入口! 设置的入口为: ' + page);
-  } // 设置状态为dom准备完毕
-
-
-  window.owo.state.isRrady = true; // 判断是否有需要运行的其他方法
-
-  if (window.owo.state.created != undefined) {
-    window.owo.state.created.forEach(function (element) {
-      // 运行对应的方法
-      element();
-    });
   }
 };
 /*
@@ -469,30 +368,25 @@ _owo.ready = function () {
  */
 
 
-_owo.whenReady = function () {
+_owo.ready = function () {
   //这个函数返回whenReady()函数
   var funcs = []; //当获得事件时，要运行的函数
-
-  var ready = false; //当触发事件处理程序时,切换为true
   //当文档就绪时,调用事件处理程序
 
   function handler(e) {
-    if (ready) return; //确保事件处理程序只完整运行一次
-    //如果发生onreadystatechange事件，但其状态不是complete的话,那么文档尚未准备好
+    // 确保事件处理程序只运行一次
+    if (window.owo.state.isRrady) return;
+    window.owo.state.isRrady = true; //如果发生onreadystatechange事件，但其状态不是complete的话,那么文档尚未准备好
 
     if (e.type === 'onreadystatechange' && document.readyState !== 'complete') {
       return;
-    } //运行所有注册函数
-    //注意每次都要计算funcs.length
-    //以防这些函数的调用可能会导致注册更多的函数
+    } // 运行所有注册函数
 
 
     for (var i = 0; i < funcs.length; i++) {
       funcs[i].call(document);
-    } //事件处理函数完整执行,切换ready状态, 并移除所有函数
+    }
 
-
-    ready = true;
     funcs = null;
   } //为接收到的任何事件注册处理程序
 
@@ -509,7 +403,7 @@ _owo.whenReady = function () {
 
 
   return function whenReady(fn) {
-    if (ready) {
+    if (window.owo.state.isRrady) {
       fn.call(document);
     } else {
       funcs.push(fn);
@@ -518,4 +412,27 @@ _owo.whenReady = function () {
 }(); // 执行页面加载完毕方法
 
 
-_owo.whenReady(_owo.ready);
+_owo.ready(_owo.showPage);
+/* 运行页面所属的方法 */
+
+
+_owo.handlePage = function (newPageFunction, entryDom) {
+  newPageFunction['$el'] = entryDom;
+  /* 判断页面是否有自己的方法 */
+
+  if (!newPageFunction) return; // console.log(newPageFunction)
+  // 如果有created方法则执行
+
+  if (newPageFunction.created) {
+    _owo.runCreated(newPageFunction);
+  } // 判断页面是否有下属模板,如果有运行所有模板的初始化方法
+
+
+  for (var key in newPageFunction.template) {
+    var templateScript = newPageFunction.template[key]; // 待修复,临时获取方式,这种方式获取到的dom不准确
+
+    var childDom = document.querySelectorAll('[template="' + key + '"]')[0]; // 递归处理
+
+    _owo.handlePage(templateScript, childDom);
+  }
+};
